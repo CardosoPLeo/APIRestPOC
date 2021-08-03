4012af2aa6b6aae77253c99906d640d2340c0ebb
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-08-03 12:19:44 -0300

    Criando Domain com cliente, adcionando referencias ao EFe e mapeando o domain como DbSet

---
+++ AR.Data/ContextoPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Data
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Cleinte { get; set; }
+
+    }
+
+
+
+}
